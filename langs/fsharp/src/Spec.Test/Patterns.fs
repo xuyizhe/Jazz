d07350eec48a2d8dea1355ad02829e76572647c1
xuyizhe <barrenbass@gmail.com>
2018-02-12 09:31:36 +0000

    Test FSharp.Spec.Patterns

--- langs/fsharp/src/Spec.Test/Patterns.fs
+++ langs/fsharp/src/Spec.Test/Patterns.fs
@@ -10,3 +10,33 @@ let ``mutipleExpr' works`` () =
   Assert.Equal(6, mutipleExpr' 3)
   Assert.Equal(64, mutipleExpr' 8)
   Assert.Equal(11, mutipleExpr' 11)
+
+[<Fact>]
+let ``recSearch' works`` () =
+  Assert.Equal("done.", recSearch' [
+    (1., 2., 0.)
+    (2., 1., 1.)
+    (3., 0., 1.)
+    ])
+
+[<Fact>]
+let ``matchNorm works`` () =
+  Assert.Equal("Versor found!", matchNorm (1., 0., 0.))
+  Assert.Equal("Simple vector with norm 2.000000", matchNorm (2., 0., 0.))
+
+[<Fact>]
+let ``VersorOrVector works`` () =
+  Assert.Equal("Versor found!", VersorOrVector (1., 0., 0.))
+  Assert.Equal("Is a Vector", VersorOrVector (2., 0., 0.))
+
+[<Fact>]
+let ``handlePowerUp works`` () =
+  Assert.Equal("Ouch, that's hot!", handlePowerUp FireFlower)
+  Assert.Equal("Please don't step on me...4", handlePowerUp (Mushroom <| Red 4))
+  Assert.Equal("Let me play some special music for you for 4 seconds.", handlePowerUp (Star 4))
+
+[<Fact>]
+let ``matchPalindrome works`` () =
+  Assert.Equal("The string aba is palindrome", matchPalindrome "aba")
+  Assert.Equal("Not a special string!", matchPalindrome "foo")
+  Assert.Equal("Hello Antonio", matchPalindrome "Antonio")
--- langs/fsharp/src/Spec/Patterns.fs
+++ langs/fsharp/src/Spec/Patterns.fs
@@ -5,3 +5,80 @@ let mutipleExpr' n =
   | 1 | 3 | 5 | 7 -> n + n
   | 2 | 4 | 6 | 8 -> n * n
   | _ -> n
+
+
+
+type MushroomColor =
+  | Red of int
+  | Green
+  | Purple
+
+type PowerUp =
+  | FireFlower
+  | Mushroom of MushroomColor
+  | Star of int
+
+let handlePowerUp = function
+  | FireFlower -> sprintf "Ouch, that's hot!"
+  | Mushroom color ->
+    match color with
+      | Red count -> sprintf "Please don't step on me...%d" count
+      | Green -> sprintf "1UP!!!"
+      | Purple -> sprintf "Sorry, about that"
+  | Star duration -> sprintf "Let me play some special music for you for %d seconds." duration
+
+
+
+let recSearch' lst =
+  let rec loop l =
+    match l with
+      | (1., _, z) :: tail ->
+        sprintf "found x=1. and z=%f" z; loop tail
+      | (2., _, _) :: tail ->
+        sprintf "found x=2."; loop tail
+      | _ :: tail -> loop tail
+      | [] -> sprintf "done."
+
+  loop lst
+
+
+
+let (|Norm|) (a:float, b:float, c:float) =
+  sqrt(a*a + b*b + c*c)
+
+let matchNorm v =
+  match v with
+  | Norm(1.) -> sprintf "Versor found!"
+  | Norm(n) -> sprintf "Simple vector with norm %f" n
+
+
+
+let (|Vector|Versor|) (a:float, b:float, c:float) =
+  match sqrt(a*a + b*b + c*c) with
+  | 1. -> Versor
+  | _ -> Vector
+
+let VersorOrVector v =
+  match v with
+  | Versor -> sprintf "Versor found!"
+  | Vector -> sprintf "Is a Vector"
+
+
+
+let rec isPalindrome (s:string) (fromidx:int) (toidx:int) =
+  if s = null then false
+  elif toidx - fromidx < 2 then true
+  elif s.[fromidx] = s.[(toidx - 1)] then
+    isPalindrome s (fromidx + 1) (toidx - 1)
+  else false
+
+let (|PALINDROME|_|) (s:string) =
+  match isPalindrome s 0 s.Length with
+  | true -> Some s
+  | false -> None
+
+let matchPalindrome v =
+  match v with
+  | PALINDROME v -> sprintf "The string %s is palindrome" v
+  | "Antonio" -> sprintf "Hello Antonio"
+  | _ -> sprintf "Not a special string!"
